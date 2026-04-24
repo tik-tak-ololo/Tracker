ce75cb2702c310ee4ebe3ffa19042e77ee47133a
tik-tak-ololo <tik-tak-ololo@outlook.com>
2026-04-24 10:28:54 +0700

    Initial commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Сергей Хмелёв on 24.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupSubviews()
+        setupConstraints()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .black
+    }
+    
+    private func setupSubviews() {
+        
+    }
+    
+    private func setupConstraints() {
+
+    }
+
+
+}
+
