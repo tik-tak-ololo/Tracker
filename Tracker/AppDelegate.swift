4ff74bfe9d4e658b6ce8fa484b9d2fd7f01d0352
tik-tak-ololo <tik-tak-ololo@outlook.com>
2026-05-24 14:48:11 +0700

    feat(Trackers): Added CoreDataStack

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    let coreDataStack: CoreDataStack = CoreDataStack.shared
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        _ = coreDataStack
         return true
     }
 
@@ -30,7 +32,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        coreDataStack.saveContext()
+    }
 
 }
 
---
+++ Tracker/Data/CoreData/CoreDataStack.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataStack.swift
+//  Tracker
+//
+//  Created by Сергей Хмелёв on 24.05.2026.
+//
+
+import CoreData
+
+final class CoreDataStack {
+
+    static let shared = CoreDataStack()
+
+    let container: NSPersistentContainer
+
+    var context: NSManagedObjectContext {
+        container.viewContext
+    }
+
+    private init() {
+        DayOfWeekTransformer.register()
+
+        container = NSPersistentContainer(name: "TrackerModel")
+
+        container.loadPersistentStores { _, error in
+            if let error {
+                assertionFailure("Core Data store failed to load: \(error)")
+            }
+        }
+
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+
+    func saveContext() {
+        guard context.hasChanges else { return }
+
+        do {
+            try context.save()
+        } catch {
+            assertionFailure("Failed to save Core Data context: \(error)")
+        }
+    }
+}
